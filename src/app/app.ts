1fcc4535cc530a0b36baa8b211c193c92ec8146b
makenzopowers-del <makenzopowers@alumnes.ilerna.com>
2026-03-13 17:07:47 +0100

    Configuración del home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Home } from './components/home/home';
 import { Footer } from './components/footer/footer';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
