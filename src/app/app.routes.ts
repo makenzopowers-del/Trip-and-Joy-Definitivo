09893b4e307e65ca6131e534d04e834757f2dffd
makenzopowers-del <makenzopowers@alumnes.ilerna.com>
2026-03-13 17:15:23 +0100

    Enlazamiento home con login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },      // Ruta por defecto (El inicio)
+    { path: 'login', component: Login } // La ruta para el login
+];
